f8b3e0bfab01565007b5049098ecd25826b57adb
Volodymyr <vova.gubash@gmail.com>
2021-04-29 05:07:09 +0300

    v1.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
+   if ( window.navigator.language.slice(0, 2) !== "it" ) {
+      let langBtn = document.querySelector('.lang-toggler-btn');
+
+      langBtn.classList.remove('d-none');      
+   }
+
    const menuBtn = document.querySelector(".menu__toggler-btn");
    if(menuBtn){
       const menuBody = document.querySelector(".menu__body");
@@ -28,70 +34,82 @@ document.addEventListener('DOMContentLoaded', (event) => {
    
    /** FAQ */
    const faqTabsContainer  = document.querySelector('.faq-block__tabs-wrapper.swiper-container'),
-         faqContentContainer = document.querySelector('.faq-block__content-wrapper.swiper-container');
-
-
-   let faqTabsSwiper = new Swiper(faqTabsContainer, {
-      
-      breakpoints: {
-         // when window width is >= 320px
-         watchSlidesVisibility: true,
-         watchSlidesProgress: true,
-         220: {
-         slidesPerView: 'auto',
-
-         spaceBetween: 20,
-         centeredSlides: true,
-
-         },
-         
-         576: {
-         slidesPerView: 4,
-         simulateTouch: true,
-         }
-      }
+   faqContentContainer = document.querySelector('.faq-block__content-wrapper.swiper-container');
+let faqTabsSwiper = new Swiper(faqTabsContainer, {
+   updateOnWindowResize: true,
    
-   });
+   breakpoints: {
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      220: {
+      slidesPerView: 'auto',
 
-   let faqContentSwiper =  new Swiper(faqContentContainer,{
-      slidesPerView:    1,
-      spaceBetween: 15,
-      simulateTouch: true,
+      centeredSlides: true,
+      },
       
-      speed: 800,
-      thumbs: {
-         swiper: faqTabsSwiper
+      576: {
+      slidesPerView: 4,
+      centeredSlides: false,
+      simulateTouch: true,
       }
-   });
-
-   if(window.matchMedia('(max-width: 576px)').matches){
-      faqContentSwiper.thumbs.swiper = '';
-      faqTabsSwiper.controller.control = faqContentSwiper;
-      faqContentSwiper.controller.control = faqTabsSwiper;
    }
 
+});
+let faqContentSwiper =  new Swiper(faqContentContainer,{
+   updateOnWindowResize: true,
+   slidesPerView:    1,
+   spaceBetween: 15,
+   simulateTouch: true,
+   autoHeight: true,
+   speed: 800,
+   thumbs: {
+      swiper: faqTabsSwiper
+   }
+});
+
+   function updateSwipers(){
+      
+
+      if(window.matchMedia('(max-width: 576px)').matches){
+         faqContentSwiper.thumbs.swiper = '';
+         faqTabsSwiper.controller.control = faqContentSwiper;
+         faqContentSwiper.controller.control = faqTabsSwiper;
+      }
+      faqContentSwiper.update();
+      faqTabsSwiper.update();
+      faqContentSwiper.updateAutoHeight();
+      faqTabsSwiper.init();
+      
+
+      faqContentSwiper.init();
+   
+   }
+   updateSwipers();
+
+
+   window.addEventListener('resize', updateSwipers);
+   window.addEventListener('deviceorientation ', updateSwipers);
+
+  
+
+   
 
    function swiperConfig (controlsLeft, controlsRight){
       return {
-         lazy: true,
+      updateOnWindowResize: true,
+      lazy: true,
 
-         navigation:{
-            nextEl: controlsRight,
-            prevEl: controlsLeft
-         },
+      initialSlide: 0,
+      navigation:{
+         nextEl: controlsRight,
+         prevEl: controlsLeft
+      },
       
-         slidesPerView: 'auto',
-         spaceBetween: 15,
-         simulateTouch: true,
-         autoHeight: true,
-         freeMode: true,
-
-         // autoplay:{
-         //    delay: 2000,
-         //    stopOnLastSlide: true,
-         //    disableOnInteraction: true,
-         // },
-         speed: 800,
+      slidesPerView: 'auto',
+      spaceBetween: 15,
+      simulateTouch: true,
+      freeMode: true,
+      speed: 800,
       };
    }
 
@@ -100,8 +118,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
    const galleryBtns = document.querySelectorAll('.category__btn'),
          categDescBlocks = document.querySelectorAll('.category__desc'); 
          
-
-
    const lazySwipers = document.querySelectorAll('.assortment__category');
 
    let windowHeight = document.documentElement.clientHeight;
@@ -166,19 +182,34 @@ document.addEventListener('DOMContentLoaded', (event) => {
          
          if (event.target.classList.contains('collapsed')) {
 
-            categories[i].querySelector('.swiper-wrapper').classList.remove('galleryMode');
+            categories[i].querySelector('.swiper-wrapper').classList.remove('galleryMode' ,'swiper-no-swiping');
             event.target.classList.remove('collapsed');
             categDescBlocks[i].classList.remove('show');
             swiperSliders[i] = new Swiper(categories[i].querySelector('.category__gallery'), swiperConfig(item.previousElementSibling, item.nextElementSibling));
             item.nextElementSibling.classList.remove('d-none');
             item.previousElementSibling.classList.remove('d-none');
-            event.target.innerHTML = 'Mostra di più';
+
+            let url = window.location.href.toString();
+            if (url.includes('en')){
+               event.target.innerHTML = 'show more';
+            }else{
+               event.target.innerHTML = 'Mostra di più';
+            }
          }
          else{
-            swiperSliders[i].destroy(true, true);
-            categories[i].querySelector('.swiper-wrapper').classList.add('galleryMode');
-            event.target.innerHTML = 'Mostra di meno';
+            swiperSliders[i].translateTo(0);
+            swiperSliders[i].destroy(true, false);
+
+            categories[i].querySelector('.swiper-wrapper').classList.add('galleryMode', 'swiper-no-swiping');
+
+            let url = window.location.href.toString();
+            if (url.includes('en')){
+               event.target.innerHTML = 'show less';
+            }else{
+               event.target.innerHTML = 'Mostra di meno';
+            }
             event.target.classList.add('collapsed');
+
             item.nextElementSibling.classList.add('d-none');
             item.previousElementSibling.classList.add('d-none');
 
@@ -187,15 +218,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
          }
       });
    });
-
-
-  
-
-
-   
-
-
-
 
 
 });
